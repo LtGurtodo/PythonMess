1dc3ce5346680b6474a7a1adf8f0d84598764cf3
Simon <watsonsemon@gmail.com>
2018-05-08 13:22:17 +1200

    added some more sentry stuff
    Added some more sentry math

--- subterfugeTesting.py
+++ subterfugeTesting.py
@@ -4,12 +4,17 @@ drillerP1 = input("How many drillers does your sub have?\n")
 # drillersP2 = input("How many drillers does your enemy have at the point?\n")
 # shields = input("How many shields do they have\n")
 
-ifSentry = input("How many sentrys do they have?\n")
+ifSentry = int(math.floor(input("How many sentrys do they have?\n")))
 
 if ifSentry > 0:
 
     x = input("How many hours will the sub be in the sentry?\n")
-
-    for i in range(x):
+    x = int(math.floor(x / 2)) * ifSentry
+    #Trigger once because you always get shot the moment you enter the sentrys range
+    for i in range(ifSentry):
         drillerP1 = drillerP1 - math.ceil(drillerP1 * 0.05)
+
+    # Trigger loop
+    for i in range(x):
+        drillerP1 = drillerP1 - math≈.ceil(drillerP1 * 0.05)
     print(drillerP1)
